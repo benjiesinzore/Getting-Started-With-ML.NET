3a578c44fae52dbe95a1927accb500ac63c2820f
Benjamin Sinzore <benjaminsinzore@gmail.com>
2023-05-02 23:00:20 +0300

    Created a model, trained and consumed it in Program.cs Class.

--- Getting_Started_With_ML_DOT_NET/Program.cs
+++ Getting_Started_With_ML_DOT_NET/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Getting_Started_With_ML_DOT_NET;
+
+Console.WriteLine("Hello, Benjamin!");
+
+//Load sample data
+var sampleData = new SentimentModel.ModelInput()
+{
+    Col0 = @"Benjamin is an amazing Developer.",
+};
+
+//Load model and predict output
+var result = SentimentModel.Predict(sampleData);
+
+// If Prediction is 1, sentiment is "Positive"; otherwise, sentiment is "Negative"
+var sentiment = result.PredictedLabel == 1 ? "Positive" : "Negative";
+Console.WriteLine($"Text: {sampleData.Col0}\nSentiment: {sentiment} {sampleData}");
+
+
